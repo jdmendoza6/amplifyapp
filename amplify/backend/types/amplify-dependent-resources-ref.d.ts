1cfb742027e5ebf5da8705d19d49e68056ab507e
EC2 Default User <ec2-user@ip-172-31-42-135.ap-southeast-1.compute.internal>
2022-05-24 15:02:31 +0000

    added notes with image functionality

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "s39d4bfc28": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
